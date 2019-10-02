e3c04b020c1b5dcc263ffb75976d68e40c1352c4
yhong <yhong@DESKTOP-P0QCHRS>
2019-10-02 15:49:10 +0900

    list 컨트롤러, 뷰

--- springTest/src/main/java/com/lizen/springTest/user/controller/UserController.java
+++ springTest/src/main/java/com/lizen/springTest/user/controller/UserController.java
@@ -1,5 +1,34 @@
 package com.lizen.springTest.user.controller;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.lizen.springTest.user.dto.UserDto;
+import com.lizen.springTest.user.service.UserService;
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value="/list.do", method=RequestMethod.GET)
+	public String list(Model model) {
+		List<UserDto> userList = userService.getList();
+		
+		model.addAttribute("userList", userList);
+		
+		return "main/user/userList";
+	}
+	
+	
+	@RequestMapping(value="/goAddPage.do", method=RequestMethod.GET)
+	public String add(Model model) {
+		return "main/user/userAdd";
+	}
 }
--- springTest/src/main/webapp/WEB-INF/views/main/user/userList.jsp
+++ springTest/src/main/webapp/WEB-INF/views/main/user/userList.jsp
@@ -1,12 +1,39 @@
 <%@ page language="java" contentType="text/html; charset=EUC-KR"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>user List</title>
 </head>
+<% String path= request.getContextPath(); %>
+<style>
+	table, tr, th, td{border-style:solid; text-align: center;}
+	table{border: 1px; border-collapse: collapse; width: 70%;}
+</style>
 <body>
-
+	<h3>이용자 목록</h3>
+	<form id="userListForm">
+		<table>
+			<tr>
+				<th>Id</th>
+				<th>Name</th>
+				<th>Address</th>
+				<th>Email</th>
+			</tr>
+			<c:forEach var="user" items="${userList }">
+				<tr>
+					<td>${user.userId }</td>
+					<td>${user.name }</td>
+					<td>${user.address }</td>
+					<td>${user.email }</td>
+				</tr>
+			</c:forEach>
+		</table>
+	</form>
+	
+	<br>
+	<a href="<%=path%>/goAddPage.do">등록자 추가</a>
 </body>
 </html>
